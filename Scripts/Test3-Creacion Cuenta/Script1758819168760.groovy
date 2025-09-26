c5148eee573f24a46175067fb10421bdee194964
Fabian <fabyusuga12@gmail.com>
2025-09-26 17:23:51 -0500

    Actualizar test suite

--- Scripts/Test3-Creacion Cuenta/Script1758819168760.groovy
+++ Scripts/Test3-Creacion Cuenta/Script1758819168760.groovy
@@ -1,24 +1,6 @@
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import internal.GlobalVariable as GlobalVariable
-// Capturar ID de cuenta y fecha
-// Mostrar en logs
-// Guardar en Global Variables
-// Guardar en archivo externo
-import java.io.FileWriter as FileWriter
+import internal.GlobalVariable as FileWriter
 
 WebUI.waitForElementVisible(findTestObject('Consultar Cuenta/Menu'), 30)
 
@@ -71,4 +53,8 @@ FileWriter writer = new FileWriter('C:/Users/user/Desktop/Cuentas Creadas Katalo
 writer.write(((('Cuenta: ' + cuentaID) + ' | Fecha: ') + fechaCreacion) + '\n')
 
 writer.close()
+
+WebUI.closeWindow()
+
+WebUI.switchToWindowIndex(0)
 
